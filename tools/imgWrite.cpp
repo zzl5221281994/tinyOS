bfb450c7814819b6d98360bf8379ab05a5e9c90e
zzl5221281994@hotmail.com <zzl1512010>
2016-03-26 19:21:57 +0800

    a new version of imgWrite,could specify writeFile

--- tools/imgWrite.cpp
+++ tools/imgWrite.cpp
@@ -2,14 +2,14 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-char* ImgPath="test.img";
+//char* ImgPath="test.img";
 int writeBuf  (char*rst ,char* dst, int len, long from         );
 int readFile  (char*path,char* buf, int len, long from         );
 int writeFile (char*path,char* buf, int len, long from         );
 int strToInt  (char*str                                        );
 int main(int argc,char**argv)
 {
-	if(argc!=5){
+	if(argc!=6){
 		printf("number of parameters wrong\n");
 		return 0;
 	}
@@ -17,13 +17,14 @@ int main(int argc,char**argv)
 	char *writeFrom_S  =argv[2];
 	char  *readFrom_S  =argv[3];
 	char  *writeLen_S  =argv[4];
+	char  *imgPath     =argv[5];
 	int  write_from_i  =strToInt(writeFrom_S ),
 	      read_from_i  =strToInt(readFrom_S  ),
 	            len_i  =strToInt(writeLen_S  );
 	char*fileBuff=(char*)malloc(sizeof(char)*len_i+1            );
 	fileBuff[len_i]='\0';
 	readFile (openFilePath,fileBuff,len_i,read_from_i           );
-	writeFile(ImgPath,     fileBuff,len_i,write_from_i          );
+	writeFile(imgPath,     fileBuff,len_i,write_from_i          );
 	return 0;
 }
 int readFile(char*path,char* buf, int len, long from            ){
