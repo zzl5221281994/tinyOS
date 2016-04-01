e947e6f60e0c382596c9cb453a1df8d803cbb77f
zzl5221281994@hotmail.com <zzl1512010>
2016-04-01 21:51:17 +0800

    c

--- boot/loader.asm
+++ boot/loader.asm
@@ -1,3 +1,25 @@
+;/************************************************************************************
+;Thu Mar 31 15:03:42 2016
+;
+;MIT License
+;Copyright (c) 2016 zhuzuolang
+;
+;Permission is hereby granted, free of charge, to any person obtaining a copy
+;of this software and associated documentation files (the "Software"), to deal
+;in the Software without restriction, including without limitation the rights
+;to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;copies of the Software, and to permit persons to whom the Software is
+;furnished to do so, subject to the following conditions:
+;The above copyright notice and this permission notice shall be included in all
+;copies or substantial portions of the Software.
+;THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;SOFTWARE.
+;************************************************************************************/
 %include "boot\boot_parameter.inc"
 %include "boot\boot_macro.inc    "
 org 0x7e00
@@ -70,7 +92,7 @@ LABEL_BEGIN:
 		AND		AX,0x0080
 		JZ		scrn320	
 		;BeginFunctionBlock		设置视频段基地址
-		MOV		EAX,[ES:DI+0x28]            
+		MOV		EAX,[ES:DI+0x28]
 		MOV		WORD [LABEL_DESC_VIDEO+2],AX
 		SHR		EAX,16D
 		MOV		BYTE [LABEL_DESC_VIDEO+4],AL
